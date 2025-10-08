359c237275597d864bd404b4f41ecca6fdafdec2
Abhijit <kumarabhijeetpanda@rediffmail.com>
2025-10-08 18:04:18 +0530

    Added Lombok

--- src/main/java/com/abhijit/ecomabhi/model/Catagories.java
+++ src/main/java/com/abhijit/ecomabhi/model/Catagories.java
@@ -5,37 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "catagories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Catagories {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long CatagoryId;
     private String CatagoryName;
-
-    public Catagories(Long catagoryId, String catagoryName) {
-        CatagoryId = catagoryId;
-        CatagoryName = catagoryName;
-    }
-
-    public Catagories() {
-
-    }
-
-    public Long getCatagoryId() {
-        return CatagoryId;
-    }
-
-    public void setCatagoryId(Long catagoryId) {
-        CatagoryId = catagoryId;
-    }
-
-    public String getCatagoryName() {
-        return CatagoryName;
-    }
-
-    public void setCatagoryName(String catagoryName) {
-        CatagoryName = catagoryName;
-    }
 }
